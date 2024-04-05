4ecd0559d24c3bfa1b44487f80784eb0a41ebd5c
VisualError <adominiqjakob1225@gmail.com>
2024-04-05 22:38:59 +0800

    Initial commit

---
+++ BaseCWInput.cs
@@ -0,0 +1,48 @@
+﻿using Zorro.Settings;
+
+namespace InputAPI
+{
+    public abstract class BaseCWInput : KeyCodeSetting
+    {
+        public void HandleKeys(Player player)
+        {
+            if (_disabled) return;
+            if (inputKey.GetKeyDown())
+            {
+                OnKeyDown(player);
+            }else if (inputKey.GetKeyUp())
+            {
+                OnKeyUp(player);
+            }else if (inputKey.GetKey())
+            {
+                OnHeld(player);
+            }
+        }
+        protected abstract void OnKeyDown(Player player);
+
+        protected abstract void OnKeyUp(Player player);
+
+        protected abstract void OnHeld(Player player);
+        internal GlobalInputHandler.InputKey inputKey;
+
+        public void Disable()
+        {
+            _disabled = true;
+        }
+
+        public void Enable()
+        {
+            _disabled = false;
+        }
+
+        public BaseCWInput()
+        {
+            inputKey = new GlobalInputHandler.InputKey();
+            
+            Plugin._inputs.Add(this);
+            _disabled = false;
+        }
+
+        protected bool _disabled;
+    }
+}
