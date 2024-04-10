9325d354844b08329b87c4459f2df9d93f5ed013
VisualError <adominiqjakob1225@gmail.com>
2024-04-11 07:13:52 +0800

    and it back

---
+++ InputAPI/Patches/PlayerInputPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+
+namespace InputAPI.Patches
+{
+    [HarmonyPatch(typeof(Player.PlayerInput))]
+    internal class PlayerInputPatch
+    {
+        [HarmonyPatch(nameof(Player.PlayerInput.SampeInput))]
+        [HarmonyPostfix]
+        private static void SampeInput(Player player)
+        {
+            foreach(var moddedInputs in Plugin._inputs) //u gly.
+            {
+                moddedInputs.HandleKeys(player);
+            }
+        }
+    }
+}
---
+++ InputAPI/Plugin.cs
@@ -0,0 +1,45 @@
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Collections.Generic;
+
+namespace InputAPI;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public static Plugin Instance { get; private set; } = null!;
+    internal new static ManualLogSource Logger { get; private set; } = null!;
+    internal static Harmony? Harmony { get; set; }
+    internal static List<BaseCWInput> _inputs = new List<BaseCWInput>();
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Instance = this;
+
+        Patch();
+
+        Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has loaded!");
+    }
+
+    internal static void Patch()
+    {
+        Harmony ??= new Harmony(MyPluginInfo.PLUGIN_GUID);
+
+        Logger.LogDebug("Patching...");
+
+        Harmony.PatchAll();
+
+        Logger.LogDebug("Finished patching!");
+    }
+
+    internal static void Unpatch()
+    {
+        Logger.LogDebug("Unpatching...");
+
+        Harmony?.UnpatchSelf();
+
+        Logger.LogDebug("Finished unpatching!");
+    }
+}
